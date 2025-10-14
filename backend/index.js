3d856385a7a139df1a856a9a50e2248302a37624
rohan099dubey <rohan03dubey@gmail.com>
2025-10-14 23:16:18 +0530

    add: post route

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,12 @@ app.get("/", (req, res) => {
     res.send("Welcome to backend server of Hostelia");
 })
 
+app.post("/create-user", (req, res) => {
+    const { name, email, password } = req.body;
+    console.log(name, email, password);
+    res.send("User created successfully");
+})
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 })
