e4e8321452a61e979279ed58d117be4c19c67363
Abhiraj Singh Chauhan <chauhanabhiraj06@gmail.com>
2025-11-02 16:06:05 +0530

    add: login page structure

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -15,6 +15,9 @@ function Navbar() {
           <li>
             <Link to="/contact">Contact</Link>
           </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
         </ul>
       </nav>
     </div>
